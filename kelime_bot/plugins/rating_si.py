13ad439e7210018d054aba726861c283d01aeabb
Bottesti <112906137+Bottesti@users.noreply.github.com>
2022-09-06 01:28:48 +0400

    Update rating_si.py

--- kelime_bot/plugins/rating_si.py
+++ kelime_bot/plugins/rating_si.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 
-@Client.on_message(filters.command("skor"))
+@Client.on_message(filters.command("qreytinq"))
 async def ratingsa(c:Client, m:Message):
     global rating
-    metin = """📝 Küresel Grup Derecelendirmesi :
+    metin = """📝 Qlobal Qrup Reytinqi:
 
 """
     eklenen = 0
